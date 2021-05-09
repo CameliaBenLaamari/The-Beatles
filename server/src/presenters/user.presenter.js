4dfd16d90332b15c4fd6e121b2d773c91ea414cb
CameliaBenLaamari <76062686+CameliaBenLaamari@users.noreply.github.com>
2021-05-09 23:13:56 +0100

    Delete server/src directory

--- server/src/presenters/user.presenter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PresenterUser = void 0;
-const presenter_generic_1 = require("./presenter.generic");
-class PresenterUser extends presenter_generic_1.Presenter {
-}
-exports.PresenterUser = PresenterUser;
-//# sourceMappingURL=user.presenter.js.map
